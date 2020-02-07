aba4d3fc160c509b3a54a8e272377a1b03695180
Umid Ismayilov <ismayilovumid97@gmail.com>
2020-02-07 17:52:03 +0000

    1st-project.java

---
+++ 1st-project.java
@@ -0,0 +1,10 @@
+package project01
+public class if
+public static void main(String[] args) {
+int a=6;
+int b=2;
+if(a>b && a%b==0){System.out.print("Result: " + a/b);}
+else if(b>a && b%a==0){System.out.print("Result: " + b/a);}
+else(a=b){System.out.print("Result: " + a/b);}    
+}
+
