6766972fcc7c41b53eafa9489cded462e59a7f02
ACER <ismayilovumid97>
2020-02-08 11:37:23 +0400

    lesson2 task if added

---
+++ Task1/src/main/java/IfTask.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class IfTask {
+    public static void main(String[] args) {
+        System.out.println("How old are you?");
+       Scanner in = new Scanner(System.in);
+        int age = in.nextInt();
+        if (age<18){
+            System.out.println("You can go to the cinema");}
+            else System.out.println("You can go to the night club");
+    }
+}
