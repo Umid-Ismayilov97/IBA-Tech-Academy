5350a9fd0ee66246f6bfe73732603c886893c1b2
ACER <ismayilovumid97>
2020-02-08 14:24:54 +0400

    lesson2 task array added

--- Task1/src/main/java/ArraysSort.java
+++ Task1/src/main/java/ArraysSort.java
@@ -2,28 +2,28 @@ public class ArraysSort {
     public static void main(String[] args) {
         int[] a = new int[30];
 
-        System.out.print("PO: ");
+        System.out.print("PE: ");
             for (int i = 0; i < a.length ; i++) {
                 a[i] = (int) (Math.random() * 200 -100);
                 if(a[i]>0 && a[i]%2==0)
                 {
                     System.out.print("[" + a[i]+ "]");
                 }}
-                System.out.print("\nPE: ");
+                System.out.print("\nPO: ");
         for (int i = 0; i < a.length ; i++) {
             if(a[i]>0 && a[i]%2!=0)
             {
                 System.out.print("[" + a[i]+ "]");
             }
         }
-        System.out.print("\nNO: ");
+        System.out.print("\nNE: ");
         for (int i = 0; i < a.length ; i++) {
             if(a[i]<0 && a[i]%2==0)
             {
                 System.out.print("[" + a[i]+ "]");
             }
         }
-        System.out.print("\nPE: ");
+        System.out.print("\nNO: ");
         for (int i = 0; i < a.length ; i++) {
             if(a[i]<0 && a[i]%2!=0)
             {
