5b073fcfd35b79886e8381da511b7971725a20ba
ACER <ismayilovumid97>
2020-02-12 17:06:43 +0400

    Homework 1 added

---
+++ Task1/src/main/java/Homework1.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Homework1 {
+    public static void main(String[] args) {
+        int number;
+        number = (int) (Math.random() * 100+1);
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter your name:");
+        String name = sc.nextLine();
+        while(true) {
+            System.out.println("Enter the number:");
+            int a = sc.nextInt();
+            if(a<number){
+                System.out.println("Your number is too small. Please, try again.");
+            }
+            else if(a>number) {
+                System.out.println("Your number is too big. Please, try again.");
+            }
+            else {
+                System.out.println("Congratulations: "+name);
+                break;
+            }
+
+        }
+    }
+}
+
