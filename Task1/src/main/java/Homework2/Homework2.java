8d2b8db913a26e632e4c32684536d92d81bd250c
ACER <ismayilovumid97>
2020-02-15 12:57:05 +0400

    Homework 2 added

--- Task1/src/main/java/Homework2/Homework2.java
+++ Task1/src/main/java/Homework2/Homework2.java
@@ -1,50 +1,46 @@
 package Homework2;
 
+import java.util.Arrays;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Homework2 {
-    final static char targetSymbol = '#';
-    final static char defaultCharacter = '-';
-    final static char shootedSymbol = '*';
-    final static char hittedSymbol = 'x';
-
     public static void main(String[] args) {
-        Scanner sc = new Scanner (System.in);
-        char [][] table = new char[6][6];
-        int xRaw = (int) (Math.random() * 5);
-        int yColumn = (int) (Math.random() * 5);
-        while(true){
-            for (int i=0; i<6; i++){
-                for (int j=0; j<6; j++){
-                    int raw=(int)(Math.random()*5);
-                    int col=(int)(Math.random()*5);
-                    char template=table[i][j];
-                    table[i][j]=table[raw][col];
-                }
-            }
-            for (int i=0; i<6; i++){
-                for (int j=0; j<6; j++){
-                    System.out.print(table[i][j]);
-                }System.out.println();
-            }
-            System.out.println("Enter raw cordinate between 1 and 5:");
-            int x = sc.nextInt();
-            System.out.println("Enter column cordinate between 1 and 5:");
-            int y = sc.nextInt();
-
-            if (x!=xRaw && y==yColumn){
-                System.out.println(shootedSymbol);}
-            else if (x==xRaw && y!=yColumn){
-                System.out.println(shootedSymbol);}
-            else if (x!=xRaw && y!=yColumn){
-                System.out.println(shootedSymbol);}
-               else {System.out.println(hittedSymbol);
-               break;}
-
-
+        Scanner sc = new Scanner(System.in);
+        String[][] array = new String[5][5];
+        for (String[] arr : array) {
+            Arrays.fill(arr, "-");
         }
+        Random TargetGenerator = new Random();
+        int TargetColumn = TargetGenerator.nextInt(5);
+        int TargetRow = TargetGenerator.nextInt(5);
+        System.out.println("All set. Get ready to rumble!");
 
+        while (true) {
+            System.out.println("Enter raw cordinate between 1 and 5:");
+            int x = sc.nextInt() - 1;
+            System.out.println("Enter column cordinate between 1 and 5:");
+            int y = sc.nextInt() - 1;
+            if (y == TargetRow && x == TargetColumn) {
+                System.out.println("You have won!");
+                array[x][y] = "x";
+                System.out.print("0 | 1 | 2 | 3 | 4 | 5 |\n" +
+                        "1 | " + array[0][0] + " | " + array[0][1] + " | " + array[0][2] + " | " + array[0][3] + " | " + array[0][4] + " |\n" +
+                        "2 | " + array[1][0] + " | " + array[1][1] + " | " + array[1][2] + " | " + array[1][3] + " | " + array[1][4] + " |\n" +
+                        "3 | " + array[2][0] + " | " + array[2][1] + " | " + array[2][2] + " | " + array[2][3] + " | " + array[2][4] + " |\n" +
+                        "4 | " + array[3][0] + " | " + array[3][1] + " | " + array[3][2] + " | " + array[3][3] + " | " + array[3][4] + " |\n" +
+                        "5 | " + array[4][0] + " | " + array[4][1] + " | " + array[4][2] + " | " + array[4][3] + " | " + array[4][4] + " |\n");
+                System.exit(0);
+            } else {
+                array[x][y] = "*";
+                System.out.print("0 | 1 | 2 | 3 | 4 | 5 |\n" +
+                        "1 | " + array[0][0] + " | " + array[0][1] + " | " + array[0][2] + " | " + array[0][3] + " | " + array[0][4] + " |\n" +
+                        "2 | " + array[1][0] + " | " + array[1][1] + " | " + array[1][2] + " | " + array[1][3] + " | " + array[1][4] + " |\n" +
+                        "3 | " + array[2][0] + " | " + array[2][1] + " | " + array[2][2] + " | " + array[2][3] + " | " + array[2][4] + " |\n" +
+                        "4 | " + array[3][0] + " | " + array[3][1] + " | " + array[3][2] + " | " + array[3][3] + " | " + array[3][4] + " |\n" +
+                        "5 | " + array[4][0] + " | " + array[4][1] + " | " + array[4][2] + " | " + array[4][3] + " | " + array[4][4] + " |\n");
+
+            }
+        }
     }
-
 }
-
