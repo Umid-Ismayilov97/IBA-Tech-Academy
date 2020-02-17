308836e120eacf8c06f0fe754712cfef8aad3583
ACER <ismayilovumid97>
2020-02-17 18:24:48 +0400

    Homework 5 updated

--- Task1/src/main/java/Homework5/Family.java
+++ Task1/src/main/java/Homework5/Family.java
@@ -15,9 +15,66 @@ public class Family {
     private ArrayList<Human> children;
 
     private Pet pet;
+    //////////////////////////////////////////
 
 
+    public Family(Human mother, Human father, ArrayList<Human> children) {
+        this.mother = mother;
+        this.father = father;
+        this.children = children;
+    }
 
+    public Human getMother() {
+
+        return mother;
+
+    }
+
+
+    public void setMother(Human mother) {
+
+        this.mother = mother;
+
+    }
+
+
+    public Human getFather() {
+
+        return father;
+
+    }
+
+
+    public void setFather(Human father) {
+
+        this.father = father;
+
+    }
+
+    public void setChildren(ArrayList<Human> children) {
+        this.children = children;
+    }
+
+    public Pet getPet() {
+
+        return pet;
+
+    }
+
+    public void setPet(Pet pet) {
+
+        this.pet = pet;
+
+    }
+        public void addChild (){
+
+    }
+    public void deleteChild (){
+
+    }
+    public void countFamily (){
+
+    }
     @Override
 
     public boolean equals(Object o) {
@@ -34,8 +91,6 @@ public class Family {
 
     }
 
-
-
     @Override
 
     public int hashCode() {
@@ -44,116 +99,13 @@ public class Family {
 
     }
 
-
-
-    public Human getMother() {
-
-        return mother;
-
-    }
-
-
-
-    public void setMother(Human mother) {
-
-        this.mother = mother;
-
-    }
-
-
-
-    public Human getFather() {
-
-        return father;
-
-    }
-
-
-
-    public void setFather(Human father) {
-
-        this.father = father;
-
-    }
-
-
-    public ArrayList<Human> getChildren() {
-
-        return children;
-
-    }
-
-
-
-    public Pet getPet() {
-
-        return pet;
-
-    }
-
-
-
-    public void setPet(Pet pet) {
-
-        this.pet = pet;
-
-    }
-
-
-
-    public Family(Human mother, Human father) {
-
-        this.mother = mother;
-
-        this.father = father;
-
-        children = new ArrayList<Human>();
-
-    }
-
-
-
-
-
-    public boolean deleteChild(int index){
-
-        if(index - 1 >= children.size()) return false;
-
-        else{
-
-            children.remove(index - 1);
-
-        }
-
-        return true;
-
-    }
-
-
-
     @Override
-
     public String toString() {
-
-        String result = String.format("Family{mother=%s, father=%s",
-
-                mother.toString(), father.toString());
-
-        if(children.size() > 0){
-
-            result += String.format(", children=%s",children.toString());
-
-        }
-
-        if(pet != null){
-
-            result += String.format(", pet=%s",pet.toString());
-
-        }
-
-        result += "}";
-
-        return result;
-
+        return "Family{" +
+                "mother=" + mother +
+                ", father=" + father +
+                ", children=" + children +
+                ", pet=" + pet +
+                '}';
     }
 }
--- Task1/src/main/java/Homework5/Human.java
+++ Task1/src/main/java/Homework5/Human.java
@@ -15,6 +15,7 @@ public class Human {
 
     private Family family;
 
+
     private String[][] schedule;
     public Human(String name, String surname, int year, int age, int iq, String[][] schedule) {
 
@@ -31,14 +32,24 @@ public class Human {
         this.schedule = schedule;
 
     }
+
+    public Human(String name, String surname, int year, int age, int iq) {
+        this.name = name;
+        this.surname = surname;
+        this.year = year;
+        this.age = age;
+        this.iq = iq;
+    }
+
     @Override
-
-    public String toString(){
-
-        return String.format("Human{name='%s', surname='%s'," +
-
-                " year=%d, iq=%d, schedule=%s",name,surname,year,iq, getScheduleAsAString());
-
+    public String toString() {
+        return "Human{" +
+                "name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", year=" + year +
+                ", age=" + age +
+                ", iq=" + iq +
+                '}';
     }
 
     public String getName() {
@@ -47,14 +58,11 @@ public class Human {
 
     }
 
-
-
     public void setName(String name) {
 
         this.name = name;
 
     }
-
 
 
     public String getSurname() {
@@ -64,13 +72,11 @@ public class Human {
     }
 
 
-
     public void setSurname(String surname) {
 
         this.surname = surname;
 
     }
-
 
 
     public int getYear() {
@@ -80,13 +86,11 @@ public class Human {
     }
 
 
-
     public void setYear(int year) {
 
         this.year = year;
 
     }
-
 
 
     public int getAge() {
@@ -96,13 +100,11 @@ public class Human {
     }
 
 
-
     public void setAge(int age) {
 
         this.age = age;
 
     }
-
 
 
     public int getIq() {
@@ -159,20 +161,15 @@ public class Human {
 
         StringBuilder sb = new StringBuilder();
 
-        for (String[] x: schedule
+        for (String[] x: schedule) {
 
-        ) {
-
-            for (String s: x
-
-            ) {
+            for (String s: x) {
 
                 sb.append(s).append(" ");
 
             }
 
             sb.append(",");
-
         }
 
         return sb.toString();
@@ -204,7 +201,6 @@ public class Human {
         return temp;
 
     }
-
 
 
     public void setSchedule(String[][] schedule) {
