82a949ad9dd139285ad9fd8f26b852da76d52f9a
ACER <ismayilovumid97>
2020-02-22 16:19:57 +0400

    Homework 2 updated

--- Task1/src/main/java/Homework2/Homework2.java
+++ Task1/src/main/java/Homework2/Homework2.java
@@ -7,39 +7,49 @@ import java.util.Scanner;
 public class Homework2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String[][] array = new String[5][5];
-        for (String[] arr : array) {
-            Arrays.fill(arr, "-");
+        String[][] array = {{"0", "1", "2", "3", "4", "5"}, {"1", "-", "-", "-", "-", "-"},
+                {"2", "-", "-", "-", "-", "-"}, {"3", "-", "-", "-", "-", "-"},
+                {"4", "-", "-", "-", "-", "-"}, {"5", "-", "-", "-", "-", "-"}
+        };
+
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length; j++) {
+                System.out.print(array[i][j] + " | ");
+            }
+
+            System.out.println();
         }
         Random TargetGenerator = new Random();
-        int TargetColumn = TargetGenerator.nextInt(5);
-        int TargetRow = TargetGenerator.nextInt(5);
+        int TargetColumn = TargetGenerator.nextInt(5) + 1;
+        int TargetRaw = TargetGenerator.nextInt(5) + 1;
         System.out.println("All set. Get ready to rumble!");
-
         while (true) {
             System.out.println("Enter raw cordinate between 1 and 5:");
-            int x = sc.nextInt() - 1;
+            int x = sc.nextInt();
             System.out.println("Enter column cordinate between 1 and 5:");
-            int y = sc.nextInt() - 1;
-            if (y == TargetRow && x == TargetColumn) {
+            int y = sc.nextInt();
+            if (x < 1 || x > 5 || y < 1 || y > 5) {
+                System.out.println("Please enter the number again.");
+            } else if (x == TargetRaw && y == TargetColumn) {
                 System.out.println("You have won!");
                 array[x][y] = "x";
-                System.out.print("0 | 1 | 2 | 3 | 4 | 5 |\n" +
-                        "1 | " + array[0][0] + " | " + array[0][1] + " | " + array[0][2] + " | " + array[0][3] + " | " + array[0][4] + " |\n" +
-                        "2 | " + array[1][0] + " | " + array[1][1] + " | " + array[1][2] + " | " + array[1][3] + " | " + array[1][4] + " |\n" +
-                        "3 | " + array[2][0] + " | " + array[2][1] + " | " + array[2][2] + " | " + array[2][3] + " | " + array[2][4] + " |\n" +
-                        "4 | " + array[3][0] + " | " + array[3][1] + " | " + array[3][2] + " | " + array[3][3] + " | " + array[3][4] + " |\n" +
-                        "5 | " + array[4][0] + " | " + array[4][1] + " | " + array[4][2] + " | " + array[4][3] + " | " + array[4][4] + " |\n");
+                for (int i = 0; i < array.length; i++) {
+                    for (int j = 0; j < array.length; j++) {
+                        System.out.print(array[i][j] + " | ");
+                    }
+
+                    System.out.println();
+                }
                 System.exit(0);
             } else {
                 array[x][y] = "*";
-                System.out.print("0 | 1 | 2 | 3 | 4 | 5 |\n" +
-                        "1 | " + array[0][0] + " | " + array[0][1] + " | " + array[0][2] + " | " + array[0][3] + " | " + array[0][4] + " |\n" +
-                        "2 | " + array[1][0] + " | " + array[1][1] + " | " + array[1][2] + " | " + array[1][3] + " | " + array[1][4] + " |\n" +
-                        "3 | " + array[2][0] + " | " + array[2][1] + " | " + array[2][2] + " | " + array[2][3] + " | " + array[2][4] + " |\n" +
-                        "4 | " + array[3][0] + " | " + array[3][1] + " | " + array[3][2] + " | " + array[3][3] + " | " + array[3][4] + " |\n" +
-                        "5 | " + array[4][0] + " | " + array[4][1] + " | " + array[4][2] + " | " + array[4][3] + " | " + array[4][4] + " |\n");
+                for (int i = 0; i < array.length; i++) {
+                    for (int j = 0; j < array.length; j++) {
+                        System.out.print(array[i][j] + " | ");
+                    }
 
+                    System.out.println();
+                }
             }
         }
     }
