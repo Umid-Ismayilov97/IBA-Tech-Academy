c1def15b50cff1eb24034eee9e54d92a4206e0db
ACER <ismayilovumid97>
2020-03-05 11:59:06 +0400

    Homework 6 test added

---
+++ Task1/src/test/java/homework6/FamilyTest.java
@@ -0,0 +1,20 @@
+package homework6;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FamilyTest {
+
+    @Test
+    void addChild() {
+    }
+
+    @Test
+    void deleteChild() {
+    }
+
+    @Test
+    void countFamily() {
+    }
+}
